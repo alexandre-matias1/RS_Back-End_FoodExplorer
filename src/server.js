5b5b74f8125c7ce35dc328262fa22fdeba31e28f
Alexandre <axdmatias310@gmail.com>
2023-04-03 02:10:38 -0300

    config AppError

--- src/server.js
+++ src/server.js
@@ -1,10 +1,36 @@
 const express = require('express');
+require("express-async-errors")
+
+
 
 const routes = require("./routes")
 const app = express();
 app.use(express.json())
 
 app.use(routes)
+
+app.use((error, request, response, next) =>{
+    if(error instanceof AppError){
+        return response.status(error.statusCode).json({
+            status:"error",
+            message: error.message
+        });
+    }
+
+    console.log(error)
+
+    return response.status(500).json({
+        status:"error",
+        message: "Internal Server Error"
+    })
+})
+
+
+
+
+
+
+
 const PORT = 3333;
 
 app.listen(PORT, ()=> 
